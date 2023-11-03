ac0ba9d8b138c76701e760763de06d27adc775cd
LuisMeraz01227 <luis_mbs05@hotmail.com>
2023-11-02 21:33:53 -0700

    Funciones, estructura y diseño del componente selector numerico

--- appMetHijTemPadre/src/app/selectornumerico/selectornumerico.component.ts
+++ appMetHijTemPadre/src/app/selectornumerico/selectornumerico.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-selectornumerico',
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./selectornumerico.component.css']
 })
 export class SelectornumericoComponent {
+
+  @Input() minimo: number = 1;
+  @Input() maximo: number = 1;
+  actual: number = 1;
+  constructor() { }
+
+  ngOnInit() {
+    this.actual = this.minimo;
+  }
+
+  incrementar() {
+    if (this.actual < this.maximo)
+      this.actual++;
+  }
+
+  decrementar() {
+    if (this.actual > this.minimo)
+      this.actual--;
+  }
+
+  fijar(v: number) {
+    if (v >= this.minimo && v <= this.maximo)
+      this.actual = v;
+  }
+
 
 }
